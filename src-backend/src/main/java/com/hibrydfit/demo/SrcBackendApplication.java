6d29afd6f866eb15d6bcdd537276aee8dad1805b
Javier Pintado Navarro <javier.pintado.external@atos.net>
2024-09-27 19:57:28 +0200

    first commit

---
+++ src-backend/src/main/java/com/hibrydfit/demo/SrcBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hibrydfit.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrcBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrcBackendApplication.class, args);
+	}
+
+}
---
+++ src-backend/src/test/java/com/hibrydfit/demo/SrcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hibrydfit.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrcBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
